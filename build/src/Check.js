6a5c6b59490645f3f9e98d67a9e097585ac76c6a
Boris Breuer <borisbreuer1@gmail.com>
2017-03-15 23:50:13 +0100

    No inline source maps.

--- build/src/Check.js
+++ build/src/Check.js
@@ -14,4 +14,3 @@
         throw new Error(`Didn't expect a to get here`);
     }
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ2hlY2suanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvQ2hlY2sudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0dBRUc7QUFDSCxBQUhBOztHQUVHLENBQ0gsTUFBTTtJQUNKOzs7OztPQUtHO0lBQ0gsTUFBTSxDQUFDLGlCQUFpQixDQUFDLEtBQVk7UUFDbkMsTUFBTSxJQUFJLEtBQUssQ0FBQyw2QkFBNkIsQ0FBQyxDQUFDO0lBQ2pELENBQUM7Q0FDRiJ9
